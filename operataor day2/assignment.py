3ffe8f995394c20e21d556e307fb788b8ce5f485
upvanbhatt <upvanbhatt@gmail.com>
2025-12-21 02:05:28 -1200

    assignment

---
+++ operataor day2/assignment.py
@@ -0,0 +1,22 @@
+#assignment operator
+num=10
+num=num+10 #10+10=20
+print("num+10:",num)
+num*=5
+print("num*5:",num)#20*5
+num/=2 #100/2
+print("num/2:",num)
+a=30
+a+=10 #30+10=40
+print("a+10:",a)
+a/=2
+print("a/2:",a) #40/2
+a%=5
+print("a%5:",a)#0.0 remainder
+b=5
+b**=2#(5)^2
+print("b**2",b)
+c=2
+c**=5
+print(c)#32
+
