ab524f2870cf44332b2eaf52b0161adf7197f350
upvanbhatt <upvanbhatt@gmail.com>
2025-12-21 01:26:50 -1200

    arithmetic

--- operataor day2/arithmetic.py
+++ operataor day2/arithmetic.py
@@ -1,3 +1,14 @@
-a=25
-b=26
+a=7
+b=3
+#addition
 print(a+b)
+#subtraction
+print(a-b)
+#multiplication
+print(a*b)
+#division
+print(a/b)
+#floor division
+print(a//b)#give value without decimal
+#modulo
+print(a%b)#remainder
