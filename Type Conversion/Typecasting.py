737801764499ea49beb6c2c9ab1c0b5ab3051046
upvanbhatt <upvanbhatt@gmail.com>
2025-12-21 17:50:41 -1200

    typeconversion

---
+++ Type Conversion/Typecasting.py
@@ -0,0 +1,17 @@
+#Typecasting
+#manually convert one type to other type
+
+a=int("2")
+b=4.25
+print(type(a))
+print(a+b)
+
+c=float("5")
+d=2.2
+print(type(c))
+print(c+d)
+
+#string type casting
+val=3.14
+val=str(val)
+print(type(val)) #str
---
+++ Type Conversion/typeconversion.py
@@ -0,0 +1,13 @@
+#Type conversion
+#python interpreter automatically convert one type to other
+a=2
+b=4.25
+
+sum=a+b  #2.0+4.25=6.25(implicit conversion)
+print(sum)
+
+a="2"
+b=4.25
+print(a+b) #error
+
+
