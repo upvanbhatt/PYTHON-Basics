38837217f60d2fa876016e72093258b1c10aba7f
upvanbhatt <upvanbhatt@gmail.com>
2025-12-21 17:00:12 -1200

    logical

---
+++ operataor day2/Logical.py
@@ -0,0 +1,22 @@
+#Logical operator( not,and,or)
+
+#not operator
+print("not operator:",not False)#opposite answer of boolean value
+print(not True)#opposite of True is False
+a=50
+b=30
+print(not(a>b))# False
+
+#and Operator
+# and operator give True value when both value is True
+
+val1= True
+val2= True
+print("and operator:",val1 and val2)
+
+#OR operator
+print("OR operator:", val1 or val2)
+
+a=50
+b=30
+print("or operatpr:",(a==b)or (a>b))
