b5aa4e25b3974edddba62b060f527fbc369c097a
upvanbhatt <upvanbhatt@gmail.com>
2026-01-28 23:45:25 -1200

    for loop

---
+++ LOOP/for loop.py
@@ -0,0 +1,19 @@
+# for loop: loops are used for sequential traversal. 
+nums = [1,2,3,4,5]
+for val in nums :
+    print(val)
+
+vaggies= [ "potato", "brinjal", "ladyfinger", "cucumber"]
+for val in vaggies:
+    print(val)
+else:
+    print("end")
+#break
+str="apnayoutube"
+for char in str:
+    if (char=='o'):
+        print("o found")
+        break
+    print(char)
+else:
+    print("end")
