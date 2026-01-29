421111f2ee18f4221e8c545cb934136cb13b12b2
upvanbhatt <upvanbhatt@gmail.com>
2026-01-29 00:48:21 -1200

    while loop

---
+++ LOOP/While.py
@@ -0,0 +1,17 @@
+count=1
+while count<=5:
+    print("hello")
+    count+=1
+print(count)
+# print numbeers from 1 to 5
+i=1
+while i<=5:
+    print(i)
+    i += 1
+print("loop ended")
+
+#reverse
+i=5
+while i>=1:
+    print(i)
+    i -= 1
