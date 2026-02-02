c2ea74faaf7e2543f58342dd25844cc8b3e7f2c6
upvanbhatt <upvanbhatt@gmail.com>
2026-02-02 05:09:14 -1200

    function2

---
+++ Function/fun2.py
@@ -0,0 +1,20 @@
+
+#to reduce redundancy we use function
+def calc_sum (a, b): # a,b are parameters
+    sum= a+b
+    print(sum)
+    return sum
+
+ 
+#more lines of code
+
+calc_sum(2 , 10) # function call
+
+#more lines of code
+calc_sum(12 , 17)
+
+# function definition
+#def calc_sum(a,b):
+#     return a+b
+#sum = calc_sum(1,2) :- function call and 1,2 are arguments
+#print(sum)
