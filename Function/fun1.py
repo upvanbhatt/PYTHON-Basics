641a177744e8c06ee3de13927a0a09a0bb4d8b7b
upvanbhatt <upvanbhatt@gmail.com>
2026-02-02 05:06:57 -1200

    function

---
+++ Function/fun1.py
@@ -0,0 +1,17 @@
+#function is a block of statement that perform a task
+a=5
+b=10
+sum = a+b
+print(sum)
+
+# more lines of code
+a=22
+b=15
+sum = a+b
+print(sum)
+
+#more lines of code
+a=9
+b=12
+sum = a+b
+print(sum)
