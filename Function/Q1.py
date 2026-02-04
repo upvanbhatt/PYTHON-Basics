f0141829d2220e824cc05b8f1a2aa495dbcf51fa
upvanbhatt <upvanbhatt@gmail.com>
2026-02-04 05:57:26 -1200

    Que1

---
+++ Function/Q1.py
@@ -0,0 +1,8 @@
+#WAF to print the length of a list (list is the parameter)
+cities=["delhi", "lucknow", "noida", "jaipur", "mumbai"]
+fruit=["apple", "banana", "mango", "grapes"]
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(fruit)
