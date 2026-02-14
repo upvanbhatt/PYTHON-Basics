ba8935e51bece20fcd19383572a31de5b0a79745
upvanbhatt <upvanbhatt@gmail.com>
2026-02-14 04:21:47 -1200

    init

--- Oops/init4.py
+++ Oops/init4.py
@@ -5,7 +5,7 @@ class Student:
         pass
 
     #parameterized constructors:- havin parameters other than self
-    def __init__(self, name,marks)
+    def __init__(self, name,marks):
        self.name=name
        self.marks= marks
        print("adding new student in database..")
