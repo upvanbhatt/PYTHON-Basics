2ddfa097a4bc45eef14133ae9d5b6a9781e3b16f
upvanbhatt <upvanbhatt@gmail.com>
2026-02-19 01:45:52 -1200

    privateattr

---
+++ Oops/private_attributes.py
@@ -0,0 +1,12 @@
+#private attributes and methods are meant to be used only within the class and are not accessible from outside the class.
+#we use double underscore (__)to make private
+
+class Account:
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass
+
+acc1 = Account("12345", "abcde")
+
+print(acc1.acc_no)
+print(acc1.__acc_pass)
