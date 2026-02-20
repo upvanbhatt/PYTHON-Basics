23eb1f1ca779d438969d95b01e841b6b8335b338
upvanbhatt <upvanbhatt@gmail.com>
2026-02-20 16:18:33 +0530

    privateattr

---
+++ Oops/private_attr2.py
@@ -0,0 +1,14 @@
+class Person:
+    __name ="anonymous"
+
+    def __hello(self):
+        print("hello person!")
+              
+    def welcome(self):
+       self.__hello()
+    
+              
+p1= Person()
+
+print(p1.welcome())
+    
