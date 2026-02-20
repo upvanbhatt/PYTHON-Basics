0b0697ddc0b830610c642fbf2766e62d600a6267
upvanbhatt <upvanbhatt@gmail.com>
2026-02-20 16:17:02 +0530

    multipleinher..

---
+++ Oops/MultiPle_inharitance.py
@@ -0,0 +1,16 @@
+# one derived class can inherit multiple base class property
+
+class A:
+    varA = "welcome to class A"
+
+class B:
+    varB = "welcome to class B"
+
+class C(A, B):
+    varC = "welcome to class C"
+
+c1 = C()
+
+print (c1.varC)
+print(c1.varB)
+print(c1.varA)
